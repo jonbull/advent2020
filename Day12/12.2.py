46d69eec9e1c512b8902b679ab1f9a29d4b30e55
Jon <jonbull@gmail.com>
2020-12-12 13:15:41 -0700

    Day 12 Part 2 Complete

--- Day12/12.2.py
+++ Day12/12.2.py
@@ -4,7 +4,7 @@ import copy
 timestart = datetime.datetime.now()
 direction = []
 
-theFile = 'part1.txt'
+theFile = 'Part1.txt'
 for row in open(theFile).read().splitlines():
     direction.append(row)
 
@@ -15,22 +15,22 @@ class boat():
         self.ew = 0
         self.orientation = 'E'
 
+    def __repr__(self):
+        print(f'Location NS={self.ns} EW={self.ew}')
+
     def rotate(self,direction,degrees):
         factor = int(degrees/90)
         for r in range(0,factor):
-            if direction == 'L':
-                if self.orientation == 'N':self.orientation = 'W'
-                elif self.orientation == 'W': self.orientation = 'S'
-                elif self.orientation == 'S': self.orientation = 'E'
-                elif self.orientation == 'E': self.orientation = 'N'
+
+            tempns = self.ns
+            tempew = self.ew
 
             if direction == 'R':
-                if self.orientation == 'N': self.orientation = 'E'
-                elif self.orientation == 'W': self.orientation = 'N'
-                elif self.orientation == 'S': self.orientation = 'W'
-                elif self.orientation == 'E': self.orientation = 'S'
-
-        return(self.orientation)
+                self.ew = tempns
+                self.ns = tempew*-1
+            if direction == 'L':
+                self.ew = tempns*-1
+                self.ns = tempew
 
     def move(self,arbor,units):
 
@@ -48,25 +48,28 @@ class boat():
             return self.ew
 
 titanic = boat();
+waypoint = boat();
+
+waypoint.ns = titanic.ns + 1
+waypoint.ew = titanic.ew + 10
 
 for instruction in direction:
 
-    #print(f'{instruction[0]} and {instruction[1:]}')
     if instruction[0] in ['N','S','E','W']:
-        titanic.move(instruction[0],int(instruction[1:]))
+        waypoint.move(instruction[0],int(instruction[1:]))
 
     if instruction[0] in ['F']:
-        titanic.move(titanic.orientation,int(instruction[1:]))
+        for i in range(0,int(instruction[1:])):
+            titanic.ns += waypoint.ns
+            titanic.ew += waypoint.ew
 
     if instruction[0] in ['L','R']:
-        titanic.rotate(instruction[0],int(instruction[1:]))
-
-    #print(f'NS = {titanic.ns} EW = {titanic.ew}')
+        waypoint.rotate(instruction[0],int(instruction[1:]))
 
 solution = abs(titanic.ew) + abs(titanic.ns)
 
 print(f"#solution: {solution}")
 print(f'#Time to complete: {datetime.datetime.now() - timestart}')
 
-#solution: 1603
-#Time to complete: 0:00:00.002024
+#solution: 52866
+#Time to complete: 0:00:00.006964
